ba8124b0e8d89de999865d023a059e4e11eed7aa
DJMax2007 <anekemakua@gmail.com>
2025-02-01 22:22:53 -0500

    Revert "Completed Code"
    This reverts commit cf61fbd8d3a882ced76e6df156c700066879cf04.

--- src/main/java/com/example/javafxapp/EmailUndefined.java
+++
@@ -1,9 +0,0 @@
-package com.example.javafxapp;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class EmailUndefined {
-    @FXML
-    private Label user;
-}
--- src/main/java/com/example/javafxapp/UserUndefined.java
+++
@@ -1,9 +0,0 @@
-package com.example.javafxapp;
-
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class UserUndefined {
-    @FXML
-    private Label user;
-}
